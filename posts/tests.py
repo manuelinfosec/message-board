9822325088df89ce7e4cfb9039931f888bafad6a
Manuel InfoSec <manuelinfosec@gmail.com>
2022-10-03 23:02:01 +0100

    Added tests for models and homepage

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
-# Create your tests here.
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="Message Board Tests")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "Message Board Tests")
+
+    def test_homepage(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "index.html")
+        self.assertContains(response, "Message Board Tests")
